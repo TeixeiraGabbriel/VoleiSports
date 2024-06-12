ed1d1f59884a982bd5fb420d98d41a9c894f0d81
DevRyanOliveira <gabriel.teixeira@sptech.school>
2024-06-12 13:16:35 -0300

    Ajustes

--- Site/src/models/aquarioModel.js
+++ Site/src/models/aquarioModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function exibirAquariosDoUsuario() {
 
-  var instrucaoSql = `SELECT date_format(quantidade.dt, '%d-%m-%Y') as 'Data_de_Cadastro', count(quantidade.dt) as Quantidade from (SELECT DATE(dtCadastro) as dt from usuarioCadastro) as quantidade group by quantidade.dt order by dt;`;
+  var instrucaoSql = `SELECT date_format(quantidade.dt, '%d-%m-%Y') as 'Data_de_Cadastro', count(quantidade.dt) as Quantidade from (SELECT DATE(dtCadastro) as dt from usuarioCadastro) as quantidade group by quantidade.dt order by dt desc;`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
